562df8a6958a68d2649ea11d45ea88cabc864c37
wahdo7 <ryanmarino99@gmail.com>
2020-02-03 20:01:31 -0500

    added prefabs and demo scene

---
+++ Desentience/Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float smoothTime = 1f;
+    public float maxSpeed = 50f;
+    public Transform desiredPose;
+
+    protected Vector3 currentPositionCorrectionVelocity;
+    protected Vector3 currentFacingCorrectionVelocity;
+
+    void LateUpdate()
+    {
+        if (desiredPose != null)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, desiredPose.position, ref currentPositionCorrectionVelocity, smoothTime, maxSpeed, Time.deltaTime);
+            transform.forward = Vector3.SmoothDamp(transform.forward, desiredPose.forward, ref currentFacingCorrectionVelocity, smoothTime, maxSpeed, Time.deltaTime);
+        }
+    }
+}
---
+++ Desentience/Assets/Scripts/BooletScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BooletScript : MonoBehaviour
+{
+    public float destroyDelay = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (destroyDelay > 0) Destroy(gameObject, destroyDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
