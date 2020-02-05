e48689d9f8a622d4f2c9bb153ee7f372aa8eb1cf
sshook98 <sscheckmate1@gmail.com>
2020-02-05 12:02:08 -0500

    Elevator Prefab and Title Scene

---
+++ Desentience/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void loadScene(string sceneName)
+    {
+        int sceneCount = SceneManager.sceneCount;
+        bool foundSceneName = false;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            if (SceneManager.GetSceneAt(i).name == "SceneName")
+            {
+                foundSceneName = true;
+                break;
+            }
+        }
+
+        if (foundSceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        } else
+        {
+            Debug.LogError("Incorrect scene name: " + sceneName);
+        }
+    }
+}
