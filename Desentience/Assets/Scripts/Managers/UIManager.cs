286fbd36c70301f72626b85598702b6553026307
Zach Hussin <zhussin@att.net>
2020-03-05 11:49:46 -0500

    Added UI Scripts

--- Desentience/Assets/Scripts/Managers/UIManager.cs
+++ Desentience/Assets/Scripts/Managers/UIManager.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// https://www.youtube.com/watch?v=hjnJnz77OVU
+
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static UIManager instance;
+
+    public Base_UIPanel firstPanel;
+    public Base_UIPanel secondPanel;
+
+    Base_UIPanel _currentPanel;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(gameObject); }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        TriggerOpenPanel(firstPanel);
     }
+
+    private void Update()
+    {
+        if (_currentPanel) _currentPanel.UpdateBehavior();
+    }
+
+    public void TriggerPanelTransition(Base_UIPanel panel)
+    {
+        TriggerOpenPanel(panel);
+    }
+
+    void TriggerOpenPanel(Base_UIPanel panel)
+    {
+        if (_currentPanel != null) TriggerClosePanel(_currentPanel);
+        _currentPanel = panel;
+        _currentPanel.OpenBehavior();
+
+    }
+
+    void TriggerClosePanel(Base_UIPanel panel)
+    {
+        panel.CloseBehavior();
+    }
+
+    public static UIManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
 }
---
+++ Desentience/Assets/Scripts/UI/Base_UIPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://www.youtube.com/watch?v=hjnJnz77OVU
+
+public class Base_UIPanel : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    public virtual void OpenBehavior()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            gameObject.SetActive(true);
+        }
+    }
+
+    public virtual void UpdateBehavior()
+    {
+
+    }
+
+    public virtual void CloseBehavior()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
