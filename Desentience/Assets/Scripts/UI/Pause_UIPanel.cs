0b099421ebe749ee593c886bf0f4f1093dc41e6d
ZHussin <zhussin@att.net>
2020-03-13 00:23:39 -0400

    Updated Sliders

--- Desentience/Assets/Scripts/UI/Pause_UIPanel.cs
+++ Desentience/Assets/Scripts/UI/Pause_UIPanel.cs
@@ -8,7 +8,9 @@ using UnityEngine.UI;
 public class Pause_UIPanel : Base_UIPanel
 {
     public Button resumeButton;
-    public Slider volumeSlider;
+    public Slider masterVolumeSlider;
+    public Slider musicVolumeSlider;
+    public Slider sfxVolumeSlider;
     public Button menuButton;
 
     public override void OpenBehavior()
@@ -17,8 +19,39 @@ public class Pause_UIPanel : Base_UIPanel
 
         resumeButton.onClick.RemoveAllListeners();
         resumeButton.onClick.AddListener(() => { ResumeButtonPressed(); });
-        volumeSlider.onValueChanged.RemoveAllListeners();
-        volumeSlider.onValueChanged.AddListener(delegate { VolumeSliderChanged(); });
+
+        bool resultMaster = AudioManager.Instance.masterAudioMixerGroup.audioMixer.GetFloat("volumeMaster", out float masterVolume);
+        if (resultMaster)
+        {
+            ChangeVolumeSliderValue(masterVolumeSlider, masterVolume);
+        } else
+        {
+            Debug.LogWarning("Failed to GetFloat(\"volumeMaster\")");
+        }
+        masterVolumeSlider.onValueChanged.RemoveAllListeners();
+        masterVolumeSlider.onValueChanged.AddListener(delegate { MasterVolumeSliderChanged(); });
+        bool resultMusic = AudioManager.Instance.musicAudioMixerGroup.audioMixer.GetFloat("volumeMusic", out float musicVolume);
+        if (resultMusic)
+        {
+            ChangeVolumeSliderValue(musicVolumeSlider, musicVolume);
+        }
+        else
+        {
+            Debug.LogWarning("Failed to GetFloat(\"volumeMusic\")");
+        }
+        musicVolumeSlider.onValueChanged.RemoveAllListeners();
+        musicVolumeSlider.onValueChanged.AddListener(delegate { MusicVolumeSliderChanged(); });
+        bool resultSfx = AudioManager.Instance.sfxAudioMixerGroup.audioMixer.GetFloat("volumeSfx", out float sfxVolume);
+        if (resultSfx)
+        {
+            ChangeVolumeSliderValue(sfxVolumeSlider, sfxVolume);
+        }
+        else
+        {
+            Debug.LogWarning("Failed to GetFloat(\"volumeSfx\")");
+        }
+        sfxVolumeSlider.onValueChanged.RemoveAllListeners();
+        sfxVolumeSlider.onValueChanged.AddListener(delegate { SfxVolumeSliderChanged(); });
         menuButton.onClick.RemoveAllListeners();
         menuButton.onClick.AddListener(() => { MenuButtonPressed(); });
     }
@@ -33,13 +66,28 @@ public class Pause_UIPanel : Base_UIPanel
         GameManager.Instance.TogglePause();
     }
 
-    void VolumeSliderChanged()
+    void MasterVolumeSliderChanged()
     {
-        AudioManager.Instance.masterVolume = volumeSlider.value;
+        AudioManager.Instance.masterAudioMixerGroup.audioMixer.SetFloat("volumeMaster", Mathf.Log10(masterVolumeSlider.value) * 20);
+    }
+
+    void MusicVolumeSliderChanged()
+    {
+        AudioManager.Instance.musicAudioMixerGroup.audioMixer.SetFloat("volumeMusic", Mathf.Log10(musicVolumeSlider.value) * 20);
+    }
+
+    void SfxVolumeSliderChanged()
+    {
+        AudioManager.Instance.sfxAudioMixerGroup.audioMixer.SetFloat("volumeSfx", Mathf.Log10(sfxVolumeSlider.value) * 20);
     }
 
     void MenuButtonPressed()
     {
         GameManager.Instance.ReturnToMainMenu();
+    }
+
+    void ChangeVolumeSliderValue(Slider slider, float volume_in_decibels)
+    {
+        slider.value = Mathf.Pow(10.0f, (volume_in_decibels / 20));
     }
 }
