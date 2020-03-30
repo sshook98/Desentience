bb483dda78dddaa38ec9800f3c72ecb33f383569
ZHussin <zhussin@att.net>
2020-03-30 15:36:31 -0400

    Added Test Scripts folder and new test scripts

---
+++ Desentience/Assets/Scripts/Test Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public Sprite icon = null;
+    public bool isStackable() {
+        return false;
+    }
+}
