ae787fdd0ae0c0b6a3137de110ea2d619f400986
ZHussin <zhussin@att.net>
2020-04-17 16:55:24 -0400

    Rockets!

--- Desentience/Assets/Scripts/Test Scripts/ProjectileWeapon.cs
+++ Desentience/Assets/Scripts/Test Scripts/ProjectileWeapon.cs
@@ -8,8 +8,10 @@ public class ProjectileWeapon : Weapon
     public GameObject projectilePrefab;
     public Transform projectileSpawnPoint;
     public float bulletSpeed;
+
+    //Don't want this
     public float bulletLifetime;
-    public int bulletDamage;
+    
     public AudioClip gunshot;
     public float timeOfLastShot;
     public float fireRate;
@@ -28,15 +30,15 @@ public class ProjectileWeapon : Weapon
         GameObject projectile = Instantiate(projectilePrefab);
         projectile.transform.position = projectileSpawnPoint.position;
         projectile.transform.rotation = projectileSpawnPoint.rotation;
-        Vector3 velocity = projectile.transform.rotation * Vector3.forward;
-        velocity.y = 0;
-        velocity = velocity.normalized * bulletSpeed;
+        Vector3 forward_vector = projectile.transform.rotation * Vector3.forward;
+        forward_vector.y = 0;
+        Vector3 velocity = forward_vector.normalized * bulletSpeed;
         Rigidbody projRb = projectile.GetComponent<Rigidbody>();
         projRb.velocity = velocity;
         
+        //Don't want this at all
         projectile.GetComponent<BooletScript>().destroyDelay = bulletLifetime;
 
-        
         AudioManager.Instance.PlayClipAtPoint(gunshot, projectileSpawnPoint.position, volume: 0.15f);
         // end of Action()
         return true;
---
+++ Desentience/Assets/Scripts/Test Scripts/RocketScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    public float radius;
+    public float force;
+    public GameObject explosionEffect;
+
+    private void Start()
+    {
+        Explode();
+    }
+
+    private void Update()
+    {
+        // Rocket trail effect. Probably using Unity Particle Pack
+    }
+
+    void Explode()
+    {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider collider in colliders)
+        {
+            Rigidbody rb = collider.attachedRigidbody;
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
