4aeee0c9590b0117da05a3c64cd3fc763a2689a9
Zach Hussin <zhussin@att.net>
2020-04-25 12:24:54 -0400

    HealthBar Stuff

--- Desentience/Assets/Scripts/Test Scripts/HealthBar.cs
+++ Desentience/Assets/Scripts/Test Scripts/HealthBar.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Healthy))]
 public class HealthBar : MonoBehaviour
 {
-    public Slider slider;
+    public Image healthFillImage;
     private float lastHealthValue = -1;
     public Healthy healthComponent;
 
     private void Awake()
     {
-        healthComponent = GetComponent<Healthy>();
+        healthComponent = GetComponentInParent<Healthy>();
     }
     private void Update()
     {
         if (lastHealthValue != healthComponent.currentHealth)
         {
-            slider.value = (healthComponent.currentHealth / healthComponent.maxHealth);
+            healthFillImage.fillAmount = (healthComponent.currentHealth / healthComponent.maxHealth);
         }
         lastHealthValue = healthComponent.currentHealth;
+        Debug.Log("lastHealthValue = " + lastHealthValue);
     }
 
 }
